0dfcad28c168c5e3c53e0d7edde1ad30be301675
Igor Czyż <igorczyz13@gmail.com>
2023-03-23 15:44:47 +0100

    This part two

--- main.js
+++ main.js
@@ -162,3 +162,35 @@ const meal = new Food('Ramenik')
 const meal2 = new Food('Green Tea')
 
 console.log(meal, meal2);
+
+// this w listenerach
+
+
+const btns = document.querySelectorAll('button')
+
+btns.forEach(btn => btn.addEventListener('click', (e) => {
+    console.log(this);
+    console.log('---');
+    console.log(e.target);
+    console.log('----');
+})
+)
+
+// this w funkcji strzałkowej
+
+const ob = {
+    number: 123,
+    showNumber() {
+        console.log(this.number);
+    },
+    showNumber2() {
+        const test = () => {
+            console.log(this.number);
+            console.log(this);
+        }
+        test()
+    }
+}
+
+ob.showNumber()
+ob.showNumber2()
